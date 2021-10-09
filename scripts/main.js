6dd35c12b785fd75d732c614ed9f261de59427b0
SMOLKEYS <jiroshimonis@gmail.com>
2021-10-09 20:49:41 +0800

    b

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,5 @@
 //vanilla modifications
 require("vanillaModifs/modifBullets");
 require("vanillaModifs/vanillaModifs");
-
-/*
-require("NTD/nya");
-require("NTD/boris");
-require("NTD/nemesis");
-*/
-
-//sus
 require("sus/susEffects");
 require("sus/susLiquids");
