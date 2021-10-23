51c1c7fc7173b651b9b40939e101f5e56d22513e
SMOLKEYS <jiroshimonis@gmail.com>
2021-10-23 16:03:58 +0800

    funi

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,36 @@ require("vanillaModif/modifBullets");
 require("vanillaModif/vanillaModifs");
 require("random/effects");
 
+let c = 0;
+
+const ntsh = () => Vars.ui.showCustomConfirm(
+  "Nothing.",
+  "Nothing.",
+  "???",
+  "Leave",
+  () => {
+    if(c == 11) {
+      Vars.ui.showErrorMessage("[red]Get out.")
+    } else {
+      ntsh();
+      let c = c + 1;
+    }
+  },
+  () => {
+    print("nothing...")
+  }
+  )
+
 //lmao
 Events.on(ClientLoadEvent, e => {
   Vars.ui.showCustomConfirm(
     "fear me",
     "UNLIMITED POWER",
-    "go away",
+    "???",
     "perhape",
     () => {
-      Threads.throwAppException(new Throwable("Deleted most recent custom game save and most recent campaign save."))
+      ntsh();
+      let c = c + 1;
     },
     () => {
       print("the j");
