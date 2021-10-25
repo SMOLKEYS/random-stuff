aed7ce4e6a1f5525d7f7c0dbf21aee1158564f43
SMOLKEYS <jiroshimonis@gmail.com>
2021-10-25 18:29:44 +0800

    moar

--- scripts/debug.js
+++ scripts/debug.js
@@ -2,15 +2,25 @@ const events = require("random/events");
 //please dont paradox
 const menuEvents = require("main");
 
-function createIngameButton(loc, buttonContent, script, sizeX, sizeY, level){
+function createTextButton(loc, buttonContent, script, sizeX, sizeY, level){
   Vars.ui.hudGroup.find(loc).row();
   Vars.ui.hudGroup.find(loc).button(buttonContent, script).size(sizeX, sizeY).padTop(level)
 }
 
+function createImageTextButton(loc, image, buttonContent, script, sizeX, sizeY, level){
+  Vars.ui.hudGroup.find(loc).row();
+  Vars.ui.hudGroup.find(loc).button(image, buttonContent, script).size(sizeX, sizeY).padTop(level)
+}
+
+function createImageButton(loc, image, script, sizeX, sizeY, level) {
+  Vars.ui.hudGroup.find(loc).row();
+  Vars.ui.hudGroup.find(loc).button(image, script).size(sizeX, sizeY).padTop(level)
+}
+
 //find code more atrocious than this, i'll wait
 Events.on(ClientLoadEvent, h => {
-  createIngameButton("minimap/position", "SHEvent", () => {events.starterEvent()}, 120, 40, 290);
-  createIngameButton("minimap/position", "SHPopup", () => {
+  createTextButton("minimap/position", "SHEvent", () => {events.starterEvent()}, 120, 40, 290);
+  createTextButton("minimap/position", "SHPopup", () => {
     let rnd = Mathf.floor(Mathf.random() * 3);
     if (rnd == 2) {
       let imageDialog = new BaseDialog("Image");
@@ -21,6 +31,9 @@ Events.on(ClientLoadEvent, h => {
       Vars.ui.showInfo(events.textArrayRandomizer());
     }
   }, 120, 40, 15);
-  createIngameButton("minimap/position", "MenuPopup", () => {menuEvents.ntsh()}, 160, 40, 15);
-  createIngameButton("minimap/position", "SIMP", () => {Groups.unit.each(a => a.apply(Vars.content.getByName(ContentType.status, "random-stuff-onlySeiSimp"), Number.MAX_VALUE))}, 90, 40, 15);
+  createTextButton("minimap/position", "MenuPopup", () => {menuEvents.ntsh()}, 160, 40, 15);
+  createTextButton("minimap/position", "SIMP", () => {Groups.unit.each(a => a.apply(Vars.content.getByName(ContentType.status, "random-stuff-onlySeiSimp"), Number.MAX_VALUE))}, 90, 40, 15);
+  createImageButton("minimap/position", Icons.star, () => {
+    Threads.throwAppException(new Throwable("star"))
+  }, 90, 90, 45);
 });
