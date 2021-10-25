1d6b369944086a007a43a361a7f28adb639144ab
SMOLKEYS <jiroshimonis@gmail.com>
2021-10-25 15:27:18 +0800

    maybe

--- scripts/debug.js
+++ scripts/debug.js
@@ -2,14 +2,14 @@ const events = require("random/events");
 //please dont paradox
 const menuEvents = require("main");
 
-function createIngameButton(loc, buttonContent, script, sizeX, sizeY, padPosition){
+function createIngameButton(loc, buttonContent, script, sizeX, sizeY, level){
   Vars.ui.hudGroup.row();
-  Vars.ui.hudGroup.find(loc).button(buttonContent, script).size(sizeX, sizeY).padPosition
+  Vars.ui.hudGroup.find(loc).button(buttonContent, script).size(sizeX, sizeY).padTop(level)
 }
 
 //find code more atrocious than this, i'll wait
 Events.on(ClientLoadEvent, h => {
-  createIngameButton("minimap/position", "SHEvent", events.starterEvent(), 120, 40, padTop(300));
+  createIngameButton("minimap/position", "SHEvent", events.starterEvent(), 120, 40, 300);
   createIngameButton("minimap/position", "SHPopup", () => {
     let rnd = Mathf.floor(Mathf.random() * 3);
     if (rnd == 2) {
@@ -20,6 +20,6 @@ Events.on(ClientLoadEvent, h => {
     } else {
       Vars.ui.showInfo(events.textArrayRandomizer());
     }
-  }, 120, 40, padTop(20));
-  createIngameButton("minimap/position", "MenuPopup", () => {menuEvents.ntsh()}, 160, 40, padTop(20))
+  }, 120, 40, 20);
+  createIngameButton("minimap/position", "MenuPopup", () => {menuEvents.ntsh()}, 160, 40, 20)
 });
