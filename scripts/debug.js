1c73770a8927afc4e522860707ed029869a8812b
SMOLKEYS <jiroshimonis@gmail.com>
2021-10-25 13:12:45 +0800

    function

--- scripts/debug.js
+++ scripts/debug.js
@@ -2,5 +2,5 @@ const events = require("random/events");
 
 Events.on(ClientLoadEvent, h => {
   Vars.ui.hudGroup.find("minimap/position").row();
-  Vars.ui.hudGroup.find("minimap/position").button("Debug", events.starterEvent()).size(120, 40).padTop(300);
+  Vars.ui.hudGroup.find("minimap/position").button("Debug", () => {events.starterEvent()}).size(120, 40).padTop(300);
 });
