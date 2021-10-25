59ba96ced08cb251d15014aa0f66ed2e7b1b93a1
SMOLKEYS <jiroshimonis@gmail.com>
2021-10-25 13:09:06 +0800

    h

--- scripts/debug.js
+++ scripts/debug.js
@@ -1,8 +1,6 @@
 const events = require("random/events");
 
 Events.on(ClientLoadEvent, h => {
-  if (OS.username == "SMOLKEYS") {
-    Vars.ui.hudGroup.find("minimap/position").row();
-    Vars.ui.hudGroup.find("minimap/position").button("Debug", events.starterEvent()).size(120, 40).padTop(300);
-  };
+  Vars.ui.hudGroup.find("minimap/position").row();
+  Vars.ui.hudGroup.find("minimap/position").button("Debug", events.starterEvent()).size(120, 40).padTop(300);
 });
