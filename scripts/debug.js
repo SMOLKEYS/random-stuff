ac3803c92aaf8b45edd72345f85ac1c6917d90f0
SMOLKEYS <jiroshimonis@gmail.com>
2021-10-25 18:48:29 +0800

    funi

--- scripts/debug.js
+++ scripts/debug.js
@@ -33,7 +33,7 @@ Events.on(ClientLoadEvent, h => {
   }, 120, 40, 15);
   createTextButton("minimap/position", "MenuPopup", () => {menuEvents.ntsh()}, 160, 40, 15);
   createTextButton("minimap/position", "SIMP", () => {Groups.unit.each(a => a.apply(Vars.content.getByName(ContentType.status, "random-stuff-onlySeiSimp"), Number.MAX_VALUE))}, 90, 40, 15);
-  createImageButton("minimap/position", Icon.star, () => {
-    Threads.throwAppException(new Throwable("star"))
-  }, 90, 90, 45);
+  createImageButton("minimap/position", Icon.hammer, () => {
+    Threads.throwAppException(new Throwable("Ilya246 has banned you from the game."))
+  }, 50, 50, 35);
 });
